e97d0600f8c0f2c9d04e39f8cc8d992fae537706
olya <olya@ivanovs.info>
2021-01-29 21:25:53 +1300

    Entry.asm

---
+++ xv6/kernel/entry.asm
@@ -0,0 +1,52 @@
+%define KERNBASE    0x80000000
+; Size of per-process kernel stack
+%define KERN_STACK_SIZE 4096
+
+; Page size extension (enables 4Mb pages)
+%define CR4_PSE     0x00000010
+; Paging
+%define CR0_PG      0x80000000
+; Write protect
+%define CR0_WP      0x00010000
+
+
+# By convention, the _start symbol specifies the ELF entry point.
+# Since we haven't set up virtual memory yet, our entry point is
+# the physical address of 'entry'.
+global _start; Jump to kernel_start and switch to high addresses
+    mov eax, kernel_start
+    jmp eax
+_start equ (entry - KERNBASE)
+extern kernel_start
+extern entry_page_dir
+
+bits 32
+
+section .text
+
+; The boot processor jumps here after executing the boot loader
+entry:
+    ; The kernel tells the paging hardware to allow super pages(4Mbyte) 
+    ; by setting the CR_PSE bit in cr4.
+    mov eax, cr4
+    or eax, CR_PSE
+    mov cr4, eax
+
+    ; Set page directory. Load the physical address of entrypgdir into control register cr3.
+    mov eax, (entry_page_dir - KERNBASE)
+    mov cr3, eax
+
+    ; To enable the paging hardware, xv6 sets the flag CR0_PG in the control register cr0.
+    mov eax, cr0
+    or eax, (CRO_PG | CR0_WP)
+    mov cr0, eax
+
+    ; Set up the stack pointer; 
+    mov eax, stack + KERN_STACK_SIZE
+
+    ; Jump to kernel_start and switch to high addresses
+    mov eax, kernel_start
+    jmp eax
+
+section .bss
+stack:      resb KERN_STACK_SIZE
---
+++ xv6/kernel/main.c
@@ -0,0 +1,11 @@
+// Page directory must be page-aligned.
+// Since we're specifying the PTE_PS flag for the directory entries,
+// it treats them as references to 4MB pages
+__attribute__((__aligned__(PAGE_SIZE))) u32 entry_page_dir[NUM_PAGE_DIR_ENTRIES] = {
+    // Map VA [0 : 4MB] to PA [0 : 4MB]
+    [0] = 0 | PTE_P | PTE_W | PTE_PS,
+
+    // Map VA [KERNBASE : KERNBASE + 4MB] to PA [0 : 4MB]
+    [KERNBASE >>
+        PDX_SHIFT] = 0 | PTE_P | PTE_W | PTE_PS,
+};
