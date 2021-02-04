92a8704829802aaeec4c840a7d77369b81c9d3d2
olya <olya@ivanovs.info>
2021-02-04 18:20:02 +1300

    Delete files

--- tmp/include/kernel/lapic.h
+++
@@ -1,6 +0,0 @@
-#ifndef XV6_LAPIC_H
-#define XV6_LAPIC_H
-
-void lapic_init();
-
-#endif  // XV6_LAPIC_H
--- tmp/include/kernel/param.h
+++
@@ -1,7 +0,0 @@
-#ifndef XV6_PARAM_H
-#define XV6_PARAM_H
-
-#define KERN_STACK_SIZE 4096  // IMPORTANT: also defined in kernel/entry.asm
-#define MAX_NUM_CPUS 8        // maximum number of CPUs supported
-
-#endif  // XV6_PARAM_H
