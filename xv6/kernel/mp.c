0bbf1c4254403c729748354c2c0dc29a35671c7d
olya <olya@ivanovs.info>
2021-02-22 22:16:14 +1300

    Complete mp_fp_struct_search

--- xv6/kernel/mp.c
+++ xv6/kernel/mp.c
@@ -33,13 +33,30 @@ static MP_FPStruct *mp_fp_struct_search()
 {
     u8 *BDA = (u8 *)P2V(0x400); // BIOS Data Area
 
-    // Check the first KB of the extended BIOS data area (EBDA)
-    u32 addr = ((BDA[0x0F] << 8) | BDA[0x0E]) << 4;
-    MP_FPStruct *result = mp_search(addr, 1024);
-    if (result)
     {
-        return result;
+        // Check the first KB of the extended BIOS data area (EBDA)
+        // 0x040E BDA (word) is EBDA base address >> 4 (usually!)
+        u32 addr = ((BDA[0x0F] << 8) | BDA[0x0E]) << 4;
+        MP_FPStruct *result = mp_search(addr, 1024);
+        if (result)
+        {
+            return result;
+        }
     }
+
+    {
+        // Check the last KB of system base memory
+        u32 addr = ((BDA[0x14] << 8) | BDA[0x13]) * 1024;
+        MP_FPStruct *result = mp_search(addr - 1024, 1024);
+        if (result)
+        {
+            return result;
+        }
+    }
+
+    // Check the BIOS ROM between 0xE0000 and 0xFFFFF
+    // TODO !!(mpsearch1(0xF0000, 0x10000))
+    return mp_search(0xE0000, 0x20000);
 }
 
 // Search for an MP configuration table.
@@ -59,6 +76,16 @@ static bool find_mp_config(MP_FPStruct **p_mp_struct, MP_ConfigTable **p_mp_tabl
 {
     // Find the FP Struct
     MP_FPStruct *mp_struct = mp_fp_struct_search();
+    if (mp_struct == 0)
+    {
+        LOG_ERROR("Unable to find the floating pointer struct");
+        return false;
+    }
+    if (mp_struct->config_table_phys_addr == 0)
+    {
+        LOG_ERROR("We do not support default MP configurations");
+        return false;
+    }
 }
 
 void mp_init()
