62953cb530b76a675834da50a881cd981b6463e8
olya <olya@ivanovs.info>
2021-02-27 23:57:51 +1300

    Add APIC registers

--- xv6/include/kernel/traps.h
+++ xv6/include/kernel/traps.h
@@ -23,3 +23,17 @@
 #define T_ALIGN 17   // aligment check
 #define T_MCHK 18    // machine check
 #define T_SIMDERR 19 // SIMD floating point error
+
+// These are arbitrarily chosen, but with care not to overlap
+// processor defined exceptions or interrupt vectors.
+#define T_SYSCALL 64  // system call
+#define T_DEFAULT 500 // catchall
+
+#define T_IRQ0 32 // IRQ 0 corresponds to int T_IRQ
+
+#define IRQ_TIMER 0
+#define IRQ_KBD 1
+#define IRQ_COM1 4
+#define IRQ_IDE 14
+#define IRQ_ERROR 19
+#define IRQ_SPURIOUS 31
--- xv6/kernel/lapic.c
+++ xv6/kernel/lapic.c
@@ -5,7 +5,37 @@
 #include "kernel/traps.h"
 
 // Local APIC registers, divided by 4 for use as u32[] indices.
-#define ID (0x0020 / 4) // Local APIC ID Register
+#define ID (0x0020 / 4)            // Local APIC ID Register
+#define VERSION (0x0030 / 4)       // Local APIC Version Register
+#define TASK_PRIORITY (0x0080 / 4) // Task Priority Register (TPR)
+#define EOI (0x00B0 / 4)
+#define SPURIOUS_INTERRUPT_VECTOR (0x00F0 / 4)
+#define UNIT_ENABLE 0x00000100
+#define ERROR_STATUS (0x0280 / 4)
+#define INIT_RESET 0x00000500
+#define STARTUP_IPI 0x00000600
+#define DELIVERY_STATUS 0x00001000
+#define ASSERT 0x00004000 // Assert interrupt (vs deassert)
+#define DEASSERT 0x00000000
+#define LEVEL_TRIGGERED 0x00008000
+#define BROADCAST 0x00080000 // Send to all APICs, including self.
+#define BUSY 0x00001000
+#define FIXED 0x00000000
+#define INTERRUPT_COMMAND_LO (0x0300 / 4) // Interrupt Command [31:0]
+#define INTERRUPT_COMMAND_HI (0x0310 / 4) // Interrupt Command [63:32]
+#define TIMER (0x0320 / 4)                // Local Vector Table 0 (TIMER)
+#define PCINT (0x0340 / 4)                // Performance Counter LVT
+#define LINT0 (0x0350 / 4)                // Local Vector Table 1 (LINT0)
+#define LINT1 (0x0360 / 4)                // Local Vector Table 2 (LINT1)
+#define ERROR (0x0370 / 4)                // Local Vector Table 3 (ERROR)
+
+#define MASKED 0x00010000   // Interrupt masked
+#define PERIODIC 0x00020000 // Periodic
+
+#define DIVIDE_COUNTS_BY_1 0x0000000B
+#define TIMER_INITIAL_COUNT (0x0380 / 4)
+#define TIMER_CURRENT_COUNT (0x0390 / 4)
+#define TIMER_DIVIDE_CONFIG (0x03E0 / 4)
 
 volatile u32 *gLAPIC; // initialised in kernel/mp.c
 
@@ -23,5 +53,7 @@ void lapic_init()
         return;
     }
 
-    lapic_write();
+    lapic_write(SPURIOUS_INTERRUPT_VECTOR, UNIT_ENABLE | (T_IRQ0 + IRQ_SPURIOUS));
+
+    // Configure timer interrupts (every 10000000 ticks at bus frequency)
 }
